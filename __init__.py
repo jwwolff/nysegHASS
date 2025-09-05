cfb50e77275a293fb76ca7c1bda558afae8a303f
jwwolff <jwfusco@me.com>
2025-09-04 22:12:57 -0400

    first commit

---
+++ __init__.py
@@ -0,0 +1,96 @@
+"""
+The nyseg custom component.
+
+This component implements scraping nyseg for daily energy values.
+
+Configuration:
+
+To use the hello_world component you will need to add the following to your
+configuration.yaml file.
+
+nyseg:
+"""
+from __future__ import annotations
+
+import asyncio
+from datetime import timedelta
+import logging
+
+import async_timeout
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.core import callback
+from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "nyseg"
+
+
+@asyncio.coroutine
+def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Setup our skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.async_set('nyseg.energyUsage', [])
+    coordinator = MyCoordinator(hass, config_entry, my_api)
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
+
+class MyCoordinator(DataUpdateCoordinator):
+    """My custom coordinator."""
+
+    def __init__(self, hass, config_entry, my_api):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="My sensor",
+            config_entry=config_entry,
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=30),
+            # Set always_update to `False` if the data returned from the
+            # api can be compared via `__eq__` to avoid duplicate updates
+            # being dispatched to listeners
+            always_update=True
+        )
+        self.my_api = my_api
+        self._device: MyDevice | None = None
+
+    async def _async_setup(self):
+        """Set up the coordinator
+
+        This is the place to set up your coordinator,
+        or to load data, that only needs to be loaded once.
+
+        This method will be called automatically during
+        coordinator.async_config_entry_first_refresh.
+        """
+        self._device = await self.my_api.get_device()
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(10):
+                # Grab active context variables to limit data required to be fetched from API
+                # Note: using context is not required if there is no need or ability to limit
+                # data retrieved from API.
+                listening_idx = set(self.async_contexts())
+                return await self.my_api.fetch_data(listening_idx)
+        except ApiAuthError as err:
+            # Raising ConfigEntryAuthFailed will cancel future updates
+            # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+            raise ConfigEntryAuthFailed from err
+        except ApiError as err:
+            raise UpdateFailed(f"Error communicating with API: {err}")
